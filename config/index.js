208c0fcbca7339d9d42401b07a2c80825bf74806
Charly_Angel <c.a.a.a.503@gmail.com>
2020-12-26 11:40:45 -0600

    feat(database): conexión la la base de datos de mongo

--- config/index.js
+++ config/index.js
@@ -5,4 +5,10 @@ if (process.env.NODE_ENV !== 'production') {
 module.exports = {
   port: process.env.PORT,
   isDev: process.env.NODE_ENV !== 'production',
+  mongo: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+  },
 };
