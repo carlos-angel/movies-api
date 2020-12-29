cd47984acd9c7c26809c15170d53af64135afee2
Charly_Angel <c.a.a.a.503@gmail.com>
2020-12-28 21:07:14 -0600

    feat(basic-strategy): strategy para la autorización

---
+++ utils/auth/strategies/basic.strategy.js
@@ -0,0 +1,24 @@
+const passport = require('passport');
+const { BasicStrategy } = require('passport-http');
+const boom = require('@hapi/Boom');
+const { verifyPassword } = require('../../helpers/passwordHandler');
+const { UserService } = require('../../../services/index');
+
+passport.use(
+  new BasicStrategy(async function (email, password, callback) {
+    const userService = new UserService();
+    try {
+      const user = await userService.getUser({ email });
+      if (!user) {
+        return callback(boom.unauthorized(), false);
+      }
+      if (!verifyPassword(user.password, password)) {
+        return callback(boom.unauthorized(), false);
+      }
+      delete user.password;
+      return callback(null, user);
+    } catch (error) {
+      return callback(error);
+    }
+  })
+);
