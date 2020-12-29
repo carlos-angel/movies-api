ef05f9e555fc389f0941c16f92aa9e417410b9f0
Charly_Angel <c.a.a.a.503@gmail.com>
2020-12-28 21:08:23 -0600

    feat(jwt-strategy): strategy para el jwt

--- utils/auth/strategies/basic.strategy.js
+++ utils/auth/strategies/basic.strategy.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
 const { BasicStrategy } = require('passport-http');
-const boom = require('@hapi/Boom');
+const boom = require('@hapi/boom');
 const { verifyPassword } = require('../../helpers/passwordHandler');
 const { UserService } = require('../../../services/index');
 
---
+++ utils/auth/strategies/jwt.strategy.js
@@ -0,0 +1,29 @@
+const passport = require('passport');
+const { Strategy, ExtractJwt } = require('passport-jwt');
+const boom = require('@hapi/boom');
+const { UserService } = require('../../../services/index');
+const { auth } = require('../../../config/index');
+
+passport.use(
+  new Strategy(
+    {
+      secretOrKey: auth.secret,
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    },
+    async function (tokenPayload, callback) {
+      const userService = new UserService();
+      try {
+        const user = await userService.getUser({ email: tokenPayload.email });
+
+        if (!user) {
+          return callback(boom.unauthorized(), false);
+        }
+
+        delete user.password;
+        return callback({ ...user, scopes: tokenPayload.scopes });
+      } catch (error) {
+        return callback(error);
+      }
+    }
+  )
+);
