c969f1ef6310ee5ee49637d568332c3b736216bf
Charly_Angel <c.a.a.a.503@gmail.com>
2020-12-29 14:50:07 -0600

    refactor(user.routes): eliminación de rutas de prueba

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   moviesApi: require('./movies.routes'),
-  userApi: require('./users.routes'),
   userMovieApi: require('./userMovie.routes'),
   authApi: require('./auth.routes'),
 };
