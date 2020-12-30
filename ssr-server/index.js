efd35f7950ada7974f0f0d46bbefcd70943b67b8
Charly_Angel <c.a.a.a.503@gmail.com>
2020-12-29 20:24:40 -0600

    perf(remember-me): implementación de recordar sesión

--- ssr-server/index.js
+++ ssr-server/index.js
@@ -4,6 +4,8 @@ const boom = require('@hapi/boom');
 const cookieParser = require('cookie-parser');
 const axios = require('axios');
 const { config } = require('./config');
+const THIRTY_DAYS_IN_SEC = 2592000;
+const TWO_HOURS_IN_SEC = 7200;
 
 const app = express();
 
@@ -16,6 +18,7 @@ require('./utils/auth/strategies/basic');
 
 /** routes */
 app.post('/auth/sign-in', async function (req, res, next) {
+  const { rememberMe } = req.body;
   passport.authenticate('basic', function (error, data) {
     try {
       if (error || !data) {
@@ -31,6 +34,7 @@ app.post('/auth/sign-in', async function (req, res, next) {
         res.cookie('token', token, {
           httpOnly: !config.dev,
           secure: !config.dev,
+          maxAge: rememberMe ? THIRTY_DAYS_IN_SEC : TWO_HOURS_IN_SEC,
         });
         res.status(200).json(user);
       });
