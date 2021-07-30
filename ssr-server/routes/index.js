7a7c63221b23a2b6bbbea278353623729c7d02bc
Charly_Angel <c.a.a.a.503@gmail.com>
2021-07-29 21:19:01 -0500

    feat: remove ssr-server

--- ssr-server/routes/index.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  authApp: require('./auth.routes'),
-  googleApp: require('./google.routes'),
-  twitterApp: require('./twitter.routes'),
-  facebookApp: require('./facebook.routes'),
-  userMovieApp: require('./userMovie.routes'),
-};
